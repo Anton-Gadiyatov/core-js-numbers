67743a0fded0181a6f26d7dfa84c0432a0d177b8
Гадиятов Антон Николаевич <AGadiyatov@IBS.RU>
2023-11-13 23:44:07 +0300

    feat: implement getFibonacciNumber function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -268,8 +268,21 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) {
+    return 0;
+  }
+  if (index === 1) {
+    return 1;
+  }
+  let prev = 1;
+  let curr = 1;
+  for (let i = 3; i <= index; i += 1) {
+    const temp = prev + curr;
+    prev = curr;
+    curr = temp;
+  }
+  return curr;
 }
 
 /**
