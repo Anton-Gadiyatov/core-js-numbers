b8cab5ad046a3c159fd0b7dec96361a74917d37c
Гадиятов Антон Николаевич <AGadiyatov@IBS.RU>
2023-11-13 23:15:28 +0300

    feat: implement toNumber function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -235,8 +235,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const number = Number.parseFloat(value);
+  return Number.isNaN(number) ? def : number;
 }
 
 /**
